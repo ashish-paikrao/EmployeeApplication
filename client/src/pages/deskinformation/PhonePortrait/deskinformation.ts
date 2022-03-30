f0e1a5cfe809e0308ed58d08c68362317e6ab5c0
Ashish Paikrao <ashish_paikrao@magicsoftware.com>
2022-03-30 09:55:39 +0530

    [Studio] Modified screen "deskInformation"

--- client/src/pages/deskinformation/PhonePortrait/deskinformation.ts
+++ client/src/pages/deskinformation/PhonePortrait/deskinformation.ts
@@ -108,7 +108,7 @@ export class deskinformation_PhonePortrait extends Screen {
           this.alert("This desk is already booked please select another desk");
       }
       else {
-          this.alert("In else");
+          this.alert(this.data.deskInfo[i].occupied);
           this.action('passData');
           
       }
