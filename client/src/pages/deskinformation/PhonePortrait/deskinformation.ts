64e77fe4a3e25eba2ed7519c27743928818eb268
Ashish Paikrao <ashish_paikrao@magicsoftware.com>
2022-03-30 10:02:21 +0530

    [Studio] Modified screen "deskInformation"

--- client/src/pages/deskinformation/PhonePortrait/deskinformation.ts
+++ client/src/pages/deskinformation/PhonePortrait/deskinformation.ts
@@ -104,12 +104,12 @@ export class deskinformation_PhonePortrait extends Screen {
       this.data.selectedDesk = this.data.deskInfo[i].deskID;
       this.data.selectedRow = this.global.row;
       this.data.selectedColumn = this.global.column;
-      if (this.data.deskInfo[i].occupied == '1')
+      if (this.data.deskInfo[i].occupied == 1)
       {
           this.alert("This desk is already booked please select another desk");
       }
       else {
-          this.alert(this.data.deskInfo[i].occupied);
+          this.alert(this.data.deskInfo[i].ocuupied );
         //   this.action('passData');
           
       }
